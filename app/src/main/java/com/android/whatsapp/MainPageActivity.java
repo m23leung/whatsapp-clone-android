993197e1bc341df4bf2a975df79772eb4caf5705
Mark Leung <mark.leung89@gmail.com>
2020-12-09 11:09:51 +1100

    Test notification

--- app/src/main/java/com/android/whatsapp/MainPageActivity.java
+++ app/src/main/java/com/android/whatsapp/MainPageActivity.java
@@ -17,6 +17,7 @@ import com.android.whatsapp.Chat.ChatListAdapter;
 import com.android.whatsapp.Chat.ChatObject;
 import com.android.whatsapp.User.UserListAdapter;
 import com.android.whatsapp.User.UserObject;
+import com.android.whatsapp.Utils.SendNotification;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +25,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.onesignal.OneSignal;
 
 import java.util.ArrayList;
 
@@ -40,6 +42,18 @@ public class MainPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
+        OneSignal.startInit(this).init();
+        OneSignal.setSubscription(true);
+        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+            @Override
+            public void idsAvailable(String userId, String registrationId) {
+                FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("notificationKey").setValue(userId);
+            }
+        });
+        OneSignal.setInFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification);
+
+        new SendNotification("message 1", "heading 1", null);
+
         Fresco.initialize((this));
 
         Button mLogout = findViewById(R.id.logout);
@@ -55,6 +69,7 @@ public class MainPageActivity extends AppCompatActivity {
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                OneSignal.setSubscription(false);
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
